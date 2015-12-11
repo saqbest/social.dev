82106d9e688cabadb353d8ec8287ba09285caacd
Sargis <saqoxachatryan1988@gmail.com>
2015-12-11 13:35:38 +0400

    asasas

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -1,8 +1,6 @@
 <?php
 
 /* @var $this yii\web\View */
-use kartik\file\fileinput;
-use yii\helpers\Url;
 
 $this->title = 'Coll social network';
 ?>
@@ -11,8 +9,7 @@ $this->title = 'Coll social network';
     <div class="jumbotron">
 
 
-
-          <h1>Welcome!</h1>
+        <h1>Welcome</h1>
           <p><a class='btn btn-lg btn-success' href='/site/login'>Login</a></p>
           OR
           <p><a class='btn btn-lg btn-success' href='/site/signup'>Signup</a></p>
